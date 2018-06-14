8ab94c27c0ee521ef01d99a16a94c8894e974ab6
zxy <1319089976@qq.com>
2018-06-14 15:26:19 +0800

    实现登录页面

---
+++ src/views/login.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="login">
+        <el-form ref="form" :rules="rules" :model="form" class="container">
+            <el-form-item>
+              <div class="avatar">
+                <img src="../assets/avatar.jpg">
+              </div>
+            </el-form-item>
+            <el-form-item prop="username">
+                <el-input v-model="form.username" placeholder="账户" prefix-icon="el-icon-tickets"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input v-model="form.password" placeholder="密码" prefix-icon="el-icon-tickets"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" class="login-btn">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ]
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.login{
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+}
+.container{
+  position: absolute;
+  left: 0;
+  right: 0;
+  width: 400px;
+  padding: 0 40px 15px 40px;
+  margin: 200px auto;
+  background-color: white;
+  .avatar {
+    position: relative;
+    left: 50%;
+    width: 120px;
+    height: 120px;
+    margin-left: -60px;
+    margin-top: -60px;
+    box-sizing: border-box;
+    border-radius: 50%;
+    border: 10px solid #fff;
+    box-shadow: 0 1px 5px #ccc;
+    overflow: hidden;
+  }
+  img{
+    width: 100%;
+    height: 100%;
+  }
+  .login-btn{
+    width: 100%;
+  }
+}
+</style>
